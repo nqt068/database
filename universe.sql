b5221e83c34516d30a77bf2e17146f67117abdc3
nqt068 <87752225+nqt068@users.noreply.github.com>
2023-04-09 00:59:38 +0700

    Update universe.sql

--- universe.sql
+++ universe.sql
@@ -5,7 +5,7 @@ USE universe;
 CREATE TABLE IF NOT EXISTS galaxy
 (
     galaxy_id SERIAL PRIMARY KEY,
-    galaxy_name VARCHAR(30) NOT NULL UNIQUE,
+    name VARCHAR(30) NOT NULL UNIQUE,
     galaxy_type TEXT NOT NULL,
     num_of_stars INT NOT NULL,
     galaxy_group VARCHAR(30)
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS galaxy
 CREATE TABLE IF NOT EXISTS quasar
 (
     quasar_id SERIAL PRIMARY KEY,
-    quasar_name VARCHAR(30) NOT NULL UNIQUE,
+    name VARCHAR(30) NOT NULL UNIQUE,
     quasar_type TEXT NOT NULL,
     galaxy_id INT REFERENCES galaxy(galaxy_id)
 );
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS quasar
 CREATE TABLE IF NOT EXISTS star
 (
     star_id SERIAL PRIMARY KEY,
-    star_name VARCHAR(30) NOT NULL UNIQUE,
+    name VARCHAR(30) NOT NULL UNIQUE,
     star_type TEXT NOT NULL,
     planets INT,
     galaxy_id INT REFERENCES galaxy(galaxy_id)
@@ -31,17 +31,17 @@ CREATE TABLE IF NOT EXISTS star
 CREATE TABLE IF NOT EXISTS planet
 (
     planet_id SERIAL PRIMARY KEY,
-    planet_name VARCHAR(30) NOT NULL UNIQUE,
+    name VARCHAR(30) NOT NULL UNIQUE,
     moons INT,
-    rings BIT NOT NULL,
+    rings BOOLEAN NOT NULL,
     star_id INT REFERENCES star(star_id)
 );
 
 CREATE TABLE IF NOT EXISTS moon
 (
     moon_id SERIAL PRIMARY KEY,
-    moon_name VARCHAR(30) NOT NULL UNIQUE,
-    sole_moon BIT NOT NULL,
+    name VARCHAR(30) NOT NULL UNIQUE,
+    sole_moon BOOLEAN NOT NULL,
     year_discovered NUMERIC(4),
     planet_id INT REFERENCES planet(planet_id)
 );
@@ -74,44 +74,38 @@ VALUES('Sol', 'Yellow Dwarf', 8, 1),
 
 INSERT INTO planet
     (planet_name, moons, rings, star_id)
-VALUES('Mercury', 0, 0, 1),
-    ('Venus', 0, 0, 1),
-    ('Earth', 1, 0, 1),
-    ('Mars', 2, 0, 1),
-    ('Jupiter', 79, 1, 1),
-    ('Saturn', 82, 1, 1),
-    ('Uranus', 27, 1, 1),
-    ('Neptune', 14, 1, 1),
-    ('Proxima b', NULL, 0, 2),
-    ('Proxima c', NULL, 0, 2),
-    ('Proxima d', NULL, 0, 2),
-    ('Barnards Star b', NULL, 0, 3);
+VALUES('Mercury', 0, FALSE, 1),
+    ('Venus', 0, FALSE, 1),
+    ('Earth', 1, FALSE, 1),
+    ('Mars', 2, FALSE, 1),
+    ('Jupiter', 79, TRUE, 1),
+    ('Saturn', 82, FALSE, 1),
+    ('Uranus', 27, TRUE, 1),
+    ('Neptune', 14, TRUE, 1),
+    ('Proxima b', NULL, FALSE, 2),
+    ('Proxima c', NULL, FALSE, 2),
+    ('Proxima d', NULL, FALSE, 2),
+    ('Barnards Star b', NULL, FALSE, 3);
 
 INSERT INTO moon
     (moon_name, sole_moon, year_discovered, planet_id)
-VALUES('Moon', 1, 1610, 3),
-    ('Phobos', 0, 1877, 4),
-    ('Deimos', 0, 1877, 4),
-    ('Ganymede', 0, 1610, 5),
-    ('Callisto', 0, 1610, 5),
-    ('Io', 0, 1610, 5),
-    ('Europa', 0, 1610, 5),
-    ('Amalthea', 0, 1892, 5),
-    ('Himalia', 0, 1904, 5),
-    ('Elara', 0, 1905, 5),
-    ('Pasiphae', 0, 1908, 5),
-    ('Sinope', 0, 1914, 5),
-    ('Lysithea', 0, 1938, 5),
-    ('Carme', 0, 1938, 5),
-    ('Ananke', 0, 1951, 5),
-    ('Leda', 0, 1974, 5),
-    ('Thebe', 0, 1979, 5),
-    ('Adrastea', 0, 1979, 5),
-    ('Metis', 0, 1979, 5),
-    ('Callirrhoe', 0, 1999, 5);
-
-
-
-
-
-
+VALUES('Moon', TRUE, 1610, 3),
+    ('Phobos', FALSE, 1877, 4),
+    ('Deimos', FALSE, 1877, 4),
+    ('Ganymede', FALSE, 1610, 5),
+    ('Callisto', FALSE, 1610, 5),
+    ('Io', FALSE, 1610, 5),
+    ('Europa', FALSE, 1610, 5),
+    ('Amalthea', FALSE, 1892, 5),
+    ('Himalia', FALSE, 1904, 5),
+    ('Elara', FALSE, 1905, 5),
+    ('Pasiphae', FALSE, 1908, 5),
+    ('Sinope', FALSE, 1914, 5),
+    ('Lysithea', FALSE, 1938, 5),
+    ('Carme', FALSE, 1938, 5),
+    ('Ananke', FALSE, 1951, 5),
+    ('Leda', FALSE, 1974, 5),
+    ('Thebe', FALSE, 1979, 5),
+    ('Adrastea', FALSE, 1979, 5),
+    ('Metis', FALSE, 1979, 5),
+    ('Callirrhoe', FALSE, 1999, 5);
